31f1da39f1a10ec03097cbd5d0426de75d0cdac8
abubakkerdev <abubakkerdev@gmail.com>
2026-02-20 23:04:34 +0600

    chnage one

--- app/page.tsx
+++ app/page.tsx
@@ -29,25 +29,10 @@ export default function Home() {
           mode: "payment",
           amount: convertToSubcurrency(amount),
           currency: "usd",
-          paymentMethodCreation: "manual", // remove this if present
-          wallets: {
-            applePay: "auto",
-            googlePay: "auto",
-          },
         }}
       >
         <CheckoutPage amount={amount} />
       </Elements>
-      {/* <Elements
-        stripe={stripePromise}
-        options={{
-          mode: "payment",
-          amount: convertToSubcurrency(amount),
-          currency: "usd",
-        }}
-      >
-        <CheckoutPage amount={amount} />
-      </Elements> */}
     </main>
   );
 }
